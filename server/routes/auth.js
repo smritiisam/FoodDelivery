970486db79cc905a55cf5ac26c15b251012970d1
SMRITI SAMIKSHYA MISHRA <smritisam2k20@gmail.com>
2025-10-29 21:45:20 +0530

    restructured for docker integration

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,20 +1,20 @@
-import express from 'express';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
-import dotenv from 'dotenv';
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import dotenv from "dotenv";
 
 const router = express.Router();
 
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
     const exisiting = await User.findOne({ email });
     if (exisiting)
-      return res.status(406).json({ message: 'User already exists' });
+      return res.status(406).json({ message: "User already exists" });
 
     const hashed = await bcrypt.hash(password, 10);
 
@@ -23,24 +23,24 @@ router.post('/signup', async (req, res) => {
     const user = new User({ name, email, password: hashed, role });
     await user.save();
 
-    res.status(201).json({ message: 'SignUp Successful' });
+    res.status(201).json({ message: "SignUp Successful" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
 
-    if (!user) return res.status(404).json({ message: 'User not found' });
+    if (!user) return res.status(404).json({ message: "User not found" });
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
-      return res.status(401).json({ message: 'Invalid Credentials' });
+      return res.status(401).json({ message: "Invalid Credentials" });
 
     const token = jwt.sign({ id: user._id, role: user.role }, JWT_SECRET, {
-      expiresIn: '1h',
+      expiresIn: "1h",
     });
 
     res.json({
@@ -56,3 +56,6 @@ export default router;
 
 //signup test:
 //  Invoke-RestMethod -Uri "http://localhost:5000/api/auth/signup" -Method POST -Headers @{ "Content-Type" = "application/json" } -Body '{"name":"Smriti","email":"smriti@example.com","password":"mysecurepassword","role":"customer"}'
+
+// signup
+// Invoke-RestMethod -Uri "http://localhost:5000/api/auth/login" -Method POST -Headers @{ "Content-Type" = "application/json" } -Body '{"email":"smriti@example.com","password":"mysecurepassword"}'
